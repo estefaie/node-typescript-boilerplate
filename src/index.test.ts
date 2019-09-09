a5a9f5e9f6758bd93567e8016457887ecb193b3a
Hemen Estefaie <estefaie@gmail.com>
2019-09-09 14:31:21 +1000

    Ignore local history

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,8 @@
-import { sum } from './index';
+import { sum } from "./index";
 
-describe('sum', () => {
-  it('should calculate the sum of two numbers correctly', () => {
+describe("sum", () => {
+  it("should calculate the sum of two numbers correctly", () => {
     expect(sum(5, 6)).toBe(11);
+    expect(sum(1.1, 3.2)).toBe(3.3);
   });
 });
