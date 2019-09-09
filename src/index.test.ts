28bb6203f44924e2000e1f8b2c257a31f5c122a7
Hemen Estefaie <estefaie@gmail.com>
2019-09-09 15:26:27 +1000

    Remove test

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,6 +3,5 @@ import { sum } from "./index";
 describe("sum", () => {
   it("should calculate the sum of two numbers correctly", () => {
     expect(sum(5, 6)).toBe(11);
-    expect(sum(1.1, 3.2)).toBe(3.3);
   });
 });
