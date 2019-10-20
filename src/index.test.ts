f40d7898d2da210a69b9aee2755708b3c0a5b86f
Hemen Estefaie <estefaie@gmail.com>
2019-10-20 22:05:27 +1100

    Update lint

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,7 @@
-import { sum } from "./index";
+import { sum } from './index';
 
-describe("sum", () => {
-  it("should calculate the sum of two numbers correctly", () => {
+describe('sum', () => {
+  it('should calculate the sum of two numbers correctly', () => {
     expect(sum(5, 6)).toBe(11);
   });
 });
